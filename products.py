7d74284120cb4b1b24299ae511f8b1a0dc8630f0
Henry Cheng <account048924@gmail.com>
2019-08-21 14:42:43 +0800

    新增split切割與存放在[]裡面

--- products.py
+++ products.py
@@ -1,5 +1,14 @@
-#記帳程式
+
+#讀取檔案(運用split切割)
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		name , price = line.strip().split(',') #用,作為分割，用strip去除\n換行
+		products.append([name, price])#print(name[0],print[0])
+	print(products)
+#記帳程式
+
+
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':	
@@ -14,9 +23,10 @@ for p in products:
 
 #若使用utf-8仍出現亂碼，用excel開啟資料>取得外部資料>從檔案，就可以完成開啟中文字。	
 with open('products.csv', 'w', encoding = 'utf-8') as f: #在python下with用法等同於有open與close功能
-
-	f.write('商品,價格,測試\n') #在csv格式之前加入名稱
+	f.write('商品,價格\n') #在csv格式之前加入名稱
 	for p in products:
 
 		f.write(p[0] + ',' + p[1] + '\n') #csv裡面都用,做為區隔
 print('檔案已寫入完成')
+
+
