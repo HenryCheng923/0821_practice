9c7f1d7cad2f83904fd014e63657eef2c76a82c5
Henry Cheng <account048924@gmail.com>
2019-08-21 14:28:23 +0800

    寫入檔案並用utf-8使用方式

--- products.py
+++ products.py
@@ -11,7 +11,12 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是', p[1])
-	
-with open('products.csv', 'w') as f: #在python下with用法等同於有open與close功能
+
+#若使用utf-8仍出現亂碼，用excel開啟資料>取得外部資料>從檔案，就可以完成開啟中文字。	
+with open('products.csv', 'w', encoding = 'utf-8') as f: #在python下with用法等同於有open與close功能
+
+	f.write('商品,價格,測試\n') #在csv格式之前加入名稱
 	for p in products:
+
 		f.write(p[0] + ',' + p[1] + '\n') #csv裡面都用,做為區隔
+print('檔案已寫入完成')
