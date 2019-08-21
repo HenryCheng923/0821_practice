6e4038995577daa38c17e82bea56d7c70457ef29
Henry Cheng <account048924@gmail.com>
2019-08-21 15:08:08 +0800

    新增import os運用os.path.isfile檢查檔案是否有在

--- products.py
+++ products.py
@@ -1,13 +1,21 @@
+import os #operating system
 
-#讀取檔案(運用split切割)
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #跳過商品與價格的資料不要寫入(跳過下一回，還在回圈內)
-		name , price = line.strip().split(',') #用,作為分割，用strip去除\n換行
-		products.append([name, price])#print(name[0],print[0])
-print(products)
+if os.path.isfile('products.csv'): #檢查相對路徑是否有此檔案
+	print('資料夾有此檔案，繼續執行步驟')
+
+	#讀取檔案(運用split切割)
+	with open('products.csv', 'r', encoding = 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #跳過商品與價格的資料不要寫入(跳過下一回，還在回圈內)
+			name , price = line.strip().split(',') #用,作為分割，用strip去除\n換行
+			products.append([name, price])#print(name[0],print[0])
+	print(products)
+
+else:
+	print('資料夾中無此檔案，無法執行')
+
 
 #讓使用者輸入
 while True:
@@ -30,5 +38,4 @@ with open('products.csv', 'w', encoding = 'utf-8') as f: #在python下with用法
 #寫入檔案
 		f.write(p[0] + ',' + p[1] + '\n') #csv裡面都用,做為區隔
 print('檔案已寫入完成')
-
 
